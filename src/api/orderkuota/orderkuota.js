118f45dfaea502347b82d341fb878c88c0772658
DanzXD <fadilhomer@gmail.com>
2025-06-06 06:11:38 +0800

    orderkuota.js

--- src/api/orderkuota/orderkuota.js
+++ src/api/orderkuota/orderkuota.js
@@ -29,7 +29,7 @@ function convertCRC16(str) {
 }
 
 function generateTransactionId() {
-    return crypto.randomBytes(5).toString('hex').toUpperCase()
+    return "DRAY" + crypto.randomBytes(5).toString('hex').toUpperCase()
 }
 
 function generateExpirationTime() {
@@ -42,7 +42,7 @@ async function elxyzFile(buffer) {
     return new Promise(async (resolve, reject) => {
         try {
 const service = new ImageUploadService('pixhost.to');
-let { directLink } = await service.uploadFromBinary(buffer, 'skyzo.png');
+let { directLink } = await service.uploadFromBinary(buffer, 'dray.png');
             resolve(directLink);
         } catch (error) {
             console.error('🚫 Upload Failed:', error);
